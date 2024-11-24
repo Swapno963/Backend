efbe9b1edc39ce96b1b1ae82a553f3c61dbf1db1
swapno963 <swapno963@gmail.com>
2024-11-24 13:55:52 +0600

    models added to admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Menu, ServiceLocation,Card,Favourite
 # Register your models here.
+admin.site.register(Menu)
+admin.site.register(ServiceLocation)
+admin.site.register(Card)
+admin.site.register(Favourite)
