5c096daf32781b86018b8f190d4d5f057f34dfb6
swapno963 <swapno963@gmail.com>
2024-11-28 13:28:39 +0600

    fixes #27

--- order/serializers.py
+++ order/serializers.py
@@ -7,6 +7,12 @@ class OrderSerializer(serializers.ModelSerializer):
 		fields = '__all__' 
 		# exclude = ['is_paid']
 
+class OrderCreateSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Order
+		fields = ['card_code','card','supplier']
+		# exclude = ['delivery_date','']
+
 
 class PaymentSerializer(serializers.ModelSerializer):
 	class Meta:
--- order/views.py
+++ order/views.py
@@ -1,15 +1,53 @@
 from django.shortcuts import render
-from .serializers import OrderSerializer, PaymentSerializer
+from .serializers import OrderSerializer, PaymentSerializer,OrderCreateSerializer
 from .models import Order, Payment
 
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
+
+import jwt
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.exceptions import AuthenticationFailed
+from django.conf import settings
 
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
-    serializer_class = OrderSerializer
+    serializer_class = OrderCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return OrderSerializer  
+        return super().get_serializer_class()
+
+
+    def create(self, request, *args, **kwargs):
+        #custom logic here
+        data = request.data.copy()
+        data['customer'] = request.user.id  # Associate the order with the logged-in user
+
+        serializer = OrderCreateSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(data, status=status.HTTP_201_CREATED)
+    
+    def perform_create(self, serializer):
+        # Save the serializer with custom logic 
+        serializer.save()
+        print("in perform create")
+
+    # Get all order of specific user
+    def list(self, request, *args, **kwargs):
+        supplier = self.request.query_params.get("supplier")  
+        orders = self.queryset.filter(supplier=supplier)  
+        serializer = self.get_serializer(orders, many=True)
+        return Response({
+            "orders": serializer.data
+        })
+    
 
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
